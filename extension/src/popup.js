a7848b709f013f519302939d7caba8bd57133834
Glavin Wiechert <glavin.wiechert@gmail.com>
2016-02-14 06:21:58 -0400

    See #7. Add Login page and improve Whitelist page
    Add "Add to Whitelist" to Whitelist page.

--- extension/src/popup.js
+++ extension/src/popup.js
@@ -1,26 +1,8 @@
 
-$(document).ready(() => {
-
-  let handlebarsSelector = 'script[type="text/x-handlebars-template"]';
-  $(handlebarsSelector).each((index, $el) => {
-    // console.log($el, $el.id);
-    var templateId = $el.id;
-    if (!templateId) {
-      return console.warn("Unknown id for element: ", $el);
-    }
-    let containerId = templateId.split('-template')[0];
-    $($el).after($(`<div id="${containerId}"></div>`));
-  });
-
-  let render = (id, context) => {
-    let el = document.getElementById(`${id}-template`);
-    let source = el.innerHTML;
-    let template = Handlebars.compile(source);
-    let html = template(context);
-    document.getElementById(id).innerHTML = html;
-  };
-
-  render('whitelist', {
+let burnt = window.burnt = {
+  templateId: 'frame',
+  __context: {
+    isLoggedIn: false,
     whitelist: [
       {
         title: 'Google',
@@ -39,6 +21,52 @@ $(document).ready(() => {
         url: 'cheatingonexams101.com'
       },
     ]
-  });
+  },
+  init() {
+    let handlebarsSelector = 'script[type="text/x-handlebars-template"]';
+    $(handlebarsSelector).each((index, $el) => {
+      // console.log($el, $el.id);
+      var templateId = $el.id;
+      if (!templateId) {
+        return console.warn("Unknown id for element: ", $el);
+      }
+      let containerId = templateId.split('-template')[0];
+      $($el).after($(`<div id="${containerId}"></div>`));
+    });
+    // Render initial page
+    burnt.refresh();
+  },
+  render(id, context) {
+    let el = document.getElementById(`${id}-template`);
+    let source = el.innerHTML;
+    let template = Handlebars.compile(source);
+    let html = template(context);
+    document.getElementById(id).innerHTML = html;
+    console.log('html', html);
+    // Bind events
+    this.bind();
+  },
+  bind() {
+    console.log('Bind element events');
+    $('#sign-in').click(() => this.login());
+  },
+  refresh() {
+    this.render(this.templateId, this.__context);
+  },
+  set(key, val) {
+    _.set(this.__context, key, val);
+    this.refresh();
+  },
+  get(key) {
+    return _.get(this.__context, key);
+  },
+  login() {
+    console.log('Login!');
+    this.set('isLoggedIn', true);
+  }
+};
 
+$(document).ready(() => {
+  console.log('Ready');
+  burnt.init();
 });
